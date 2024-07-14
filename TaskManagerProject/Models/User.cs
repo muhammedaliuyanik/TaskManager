6e731eeab1ce2117bba8936e83fbde8b95c19108
muhammedaliuyanik <muhammedaliuyank16@gmail.com>
2024-07-14 03:19:07 +0300

    create task.cs and user.cs

---
+++ TaskManagerProject/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerProject.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Password { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        public UserRole Role { get; set; }
+
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+
+    public enum UserRole
+    {
+        Admin,
+        ProjectManager,
+        Employee
+    }
+}
